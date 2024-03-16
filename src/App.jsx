dd8850d80965bb5b7761dc2098884e234e7c4b81
arijitganguly1748 <arijeetganguli32@gmail.com>
2024-03-17 01:39:33 +0530

    Setup a new project with vite and material ui, navbar and postcard is also added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from '@mui/material'
+import NavBar from './components/NavBar'
+import SocialApp from './components/SocialApp'
+function App() {
+
+  return (
+    <>
+      <SocialApp />
+    </>
+  )
+}
+
+export default App
